11fe99c6e9a4287161d19fc54cc2b145d33911f8
Selected Habibi <habibi@avang.ir>
2023-09-09 09:25:44 +0330

    End Of Aggregate

--- AggregatesSamples.Introduction/Orders/OrderLines.cs
+++ AggregatesSamples.Introduction/Orders/OrderLines.cs
@@ -1,10 +1,20 @@
 ﻿namespace AggregatesSamples.Introduction.Orders
 {
-    public class OrderLines
+    public class OrderLine
     {
         public int LineId { get; set; }
         public int OrderId { get; set; }
         public int ProductId { get; set; }
         public int Price { get; set; }
+        public int Count { get; set; }
+
+        public void ChangeCount(int NewCount)
+        {
+            if (NewCount < 1)
+                throw new ArgumentException();
+            Count = NewCount;
+        }
+
+        public int GetTotalPrice() => Price * Count;
     }
 }
--- AggregatesSamples.Introduction/Orders/Orders.cs
+++ AggregatesSamples.Introduction/Orders/Orders.cs
@@ -7,17 +7,30 @@ using System.Threading.Tasks;
 
 namespace AggregatesSamples.Introduction.Orders
 {
-    public class Orders
+    public class Order
     {
         public int OrderId { get; set; }
         public DateTime OrderDate { get; set; }
+        public int AddressLineId { get; set; }
 
-        private readonly List<OrderLines> _orderLines = new();
-        public IReadOnlyList<OrderLines> OrderLines => _orderLines;
+        private readonly List<OrderLine> _orderLines = new();
 
         public int GetTotalPrice()
         {
-            return _orderLines.Where(p => p.OrderId == OrderId).Sum(p => p.Price);
+            return _orderLines.Where(p => p.OrderId == OrderId).Sum(p => p.GetTotalPrice());
         }
+
+        public Order(int addressLineId,  DateTime orderDate, List<OrderLine> orderLines)
+        {
+            if (addressLineId < 1)
+                throw new ArgumentException();
+            if(orderLines == null || !orderLines.Any())
+                throw new ArgumentException();
+            AddressLineId = addressLineId;
+            OrderDate = orderDate;
+            _orderLines = orderLines;
+        }
+
+
     }
 }
